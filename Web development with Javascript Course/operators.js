6fce7e3ba24fea5c29feb9b91536444d01a5b970
Milana <Milana.zaborovska@gmail.com>
2022-03-07 23:56:38 +0000

    used assignement operators

---
+++ Web development with Javascript Course/operators.js
@@ -0,0 +1,25 @@
+// + addition
+// - subtraction
+// * multiplication
+// / division
+// % modulus
+// ++ increment
+// -- decrement
+
+var x = 6;
+var y = 2;
+
+x += y; // adds y to the x variable
+console.log(x);
+
+x-= y; // takes y away from x
+console.log(x);
+
+x *= y; // times x by y and assigns it to x
+console.log(x);
+
+x /= y; // divides x by y (12 by 2))
+console.log(x);
+
+x %= y; // provides the remainder of 6 / 2 (0)
+console.log(x)
