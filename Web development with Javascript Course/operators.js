0e75c0154373dcd3fedbba2b141fe8d364279982
Milana <Milana.zaborovska@gmail.com>
2022-03-08 12:29:19 +0000

    used typeof operator

--- Web development with Javascript Course/operators.js
+++ Web development with Javascript Course/operators.js
@@ -22,16 +22,19 @@ x /= y; // divides x by y (12 by 2))
 console.log(x);
 
 x %= y; // provides the remainder of 6 / 2 (0)
-console.log(x)
+console.log(x);
 
-text1 = "what a very "
-text2 = "nice day!"
+text1 = "what a very ";
+text2 = "nice day!";
 text1 += text2;
-console.log(text1) // addition with strings
+console.log(text1); // addition with strings
 
 // These are connectors:
 
-console.log(5 == 5 && 10 == 9)
+console.log(5 == 5 && 10 == 9);
 
-console.log(5 == 5 || 10 == 9) // this is true because were using 'or'
+console.log(5 == 5 || 10 == 9); // this is true because were using 'or'
 
+// this is the type of operator, it gives you the type of a variable
+
+console.log(typeof(x)); // it returns number as x is assigned to a number
