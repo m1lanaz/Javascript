b70359bd345aa036d092a1a677a70f2e0eef212a
Milana <Milana.zaborovska@gmail.com>
2022-03-08 12:45:53 +0000

    created arrays and objects

--- datatypes.js
+++ datatypes.js
@@ -2,4 +2,15 @@
 
  var x = 5 == 5;
  console.log(x);
- 
+
+ // arrays; a group of values to a variable
+
+ var foods = ["pizza", "chips", "apple"]
+ console.log(foods);
+ console.log(foods[0]); // logs the first value
+
+ // objects; written with curly braces, written as name:value pairs
+
+ var pizza = {type:"cheese", price:"£300", yummy:"no"};
+console.log(pizza.type);
+console.log(pizza);
