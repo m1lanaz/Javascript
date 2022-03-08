a03059da412c2108a439d33f14caa127d482ccfe
Milana <Milana.zaborovska@gmail.com>
2022-03-08 13:05:24 +0000

    added another global var plus spacing

--- Web development with Javascript Course/scope.js
+++ Web development with Javascript Course/scope.js
@@ -3,6 +3,9 @@
 // code here can NOT use y
 function myFunction() {
     var y = 0;
+};
+
+
 
 //Global:
 
@@ -11,4 +14,16 @@ var x = 6;
 
 function myFunction() {
     // code here can also use x
-}
+};
+
+
+
+// if you assign a value to a variable that has not been declared, it will automatically become a global variable
+
+randomFunction();
+
+// code here can use m
+
+function randomFunction() {
+     m = 3;
+};
