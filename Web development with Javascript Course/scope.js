d69cd697d78c1186270f1563767b4af7ba595e2e
Milana <Milana.zaborovska@gmail.com>
2022-03-08 13:02:41 +0000

    created a local and global variable

---
+++ Web development with Javascript Course/scope.js
@@ -0,0 +1,14 @@
+//Local:
+
+// code here can NOT use y
+function myFunction() {
+    var y = 0;
+
+//Global:
+
+var x = 6;
+// code here can use x
+
+function myFunction() {
+    // code here can also use x
+}
