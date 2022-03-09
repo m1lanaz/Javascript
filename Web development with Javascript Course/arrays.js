5fe7f19aed1c1b4c66d7832919cd22e7fb82683c
Milana <Milana.zaborovska@gmail.com>
2022-03-09 16:50:53 +0000

    learnt to add elements at certain spaces in an array

--- Web development with Javascript Course/arrays.js
+++ Web development with Javascript Course/arrays.js
@@ -1,2 +1,11 @@
 var fruits = ["apples", "bananas","pears","grapes","kiwi"];
-fruits.push("plum") // this adds plum to the end of the array 
+
+fruits.push("plum"); // this adds plum to the end of the array 
+
+fruits[7] = "lemon"; // adds a new element to fruits
+console.log(fruits[6]) // this would be undefinded since we have left a gap in our array due to setting lemon to 7
+
+console.log(fruits[0]);
+fruits[0] = "Mango"; // this will replace element at index 0 in the fruits array (it will overwrite apples as mango)
+console.log(fruits[0]);
+
