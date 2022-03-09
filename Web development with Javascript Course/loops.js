f561f365d72ef353f2baccf5d0b1cc1065d1bf04
Milana <Milana.zaborovska@gmail.com>
2022-03-09 17:56:04 +0000

    created a for and while loop

---
+++ Web development with Javascript Course/loops.js
@@ -0,0 +1,9 @@
+for(i = 0; i < 5; i++){ // statement1 is executed once before the execution of the code block, statement2 defines the condition, statement3 is executed everytime
+    console.log("The number is " + i);
+}; //it will stop before 5
+
+var x = 1
+while(x < 10) {
+    console.log("The number is " + x);
+    x ++
+}
