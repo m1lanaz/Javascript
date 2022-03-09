17b75c60dbc870f0f509916d1e1c43ffe1ee0d96
Milana <Milana.zaborovska@gmail.com>
2022-03-09 17:10:37 +0000

    created an if, else if and else statement

--- Web development with Javascript Course/conditionals.js
+++ Web development with Javascript Course/conditionals.js
@@ -4,3 +4,12 @@ if (x > 5){
 } else{
     console.log("x is less than 5!");
 };
+
+var age = 18;
+if (age >= 18){
+    console.log("You are an adult!");
+} else if (age >= 13){
+    console.log("You are a teenager!");
+} else{
+    console.log("You are a child!");
+};
