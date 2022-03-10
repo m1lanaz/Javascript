03e11c480131f21e2f8766046c3e39441928d58c
Milana <Milana.zaborovska@gmail.com>
2022-03-10 09:55:56 +0000

    used .append

--- Web development with Javascript Course/JQuery/script.js
+++ Web development with Javascript Course/JQuery/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
     $("li").eq(1).css("color", "red"); // selects the second list item
     $("h1").css("color","pink");
+
+    $("p").append("<li>Appended text</li>"); //adds text to the end
 })
