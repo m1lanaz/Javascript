1e3eb9fc7d817621530ad4e45ee272e80a59a834
Milana <Milana.zaborovska@gmail.com>
2022-03-11 23:56:21 +0000

    made the background colour for buttons transparent

--- Web development with Javascript Course/Project/script.js
+++ Web development with Javascript Course/Project/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
     $("#btn1").click(function(){
-        $("body").css("background-color",("#" + ((1<<24)*Math.random() | 0).toString(16))); //the second part of the code is a colour randomiser
+        $("body").css("background-color",("#" + ((1<<24)*Math.random() | 0).toString(16))); //the second part of the parameters is a colour randomiser
     })
 
-    
+    $("#btn2").click(function(){
+        $("h1").css("color", ("#" + ((1<<24)*Math.random() | 0).toString(16)))
+    })
 })
