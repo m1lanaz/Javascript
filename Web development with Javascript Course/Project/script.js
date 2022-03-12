c420920db32bb5ed76beeebd23e9f2c16e549194
Milana <Milana.zaborovska@gmail.com>
2022-03-12 00:17:01 +0000

    button 3 translates

--- Web development with Javascript Course/Project/script.js
+++ Web development with Javascript Course/Project/script.js
@@ -5,6 +5,30 @@ $(document).ready(function(){
     })
 
     $("#btn2").click(function(){
-        $("h1").css("color", ("#" + ((1<<24)*Math.random() | 0).toString(16)))
+        $("h1").css("color", ("#" + ((1<<24)*Math.random() | 0).toString(16)));
+    })
+
+    $("#btn3").click(function(){
+        switch(Math.floor(Math.random()*7)){
+            case 1:
+                $("h1").attr("#disappear");
+                break
+            case 2:
+                $("#btn1").attr("#disappear");
+                break
+            case 3:
+                $("#btn2").attr("#disappear");
+                break
+            case 4:
+                $("h1").removeAttr("#disappear");
+                break
+            case 5:
+                $("#btn1").removeAttr("#disappear");
+                break
+            case 6:
+                $("#btn2").removeAttr("#disappear");
+                break  
+        }
+
     })
 })
