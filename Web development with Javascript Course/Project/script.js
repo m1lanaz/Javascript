8393e59ae7d2b4d5e3715e24285dc5022e1989f0
Milana <Milana.zaborovska@gmail.com>
2022-03-12 00:22:57 +0000

    completed last button

--- Web development with Javascript Course/Project/script.js
+++ Web development with Javascript Course/Project/script.js
@@ -9,26 +9,6 @@ $(document).ready(function(){
     })
 
     $("#btn3").click(function(){
-        switch(Math.floor(Math.random()*7)){
-            case 1:
-                $("h1").attr("#disappear");
-                break
-            case 2:
-                $("#btn1").attr("#disappear");
-                break
-            case 3:
-                $("#btn2").attr("#disappear");
-                break
-            case 4:
-                $("h1").removeAttr("#disappear");
-                break
-            case 5:
-                $("#btn1").removeAttr("#disappear");
-                break
-            case 6:
-                $("#btn2").removeAttr("#disappear");
-                break  
-        }
-
+        $("button").css("background-color", ("#" + ((1<<24)*Math.random() | 0).toString(16)));
     })
 })
