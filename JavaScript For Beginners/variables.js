5dc406b693734236e091051576beb5e8b712544f
Milana Zaborovska <Milana.zaborovska@gmail.com>
2022-03-17 09:37:52 +0000

    added variables and reassigned variables

--- JavaScript For Beginners/variables.js
+++ JavaScript For Beginners/variables.js
@@ -2,3 +2,11 @@ var name = "Milana";
 var course = "Javascript for beginners";
 
 alert(course);
+
+course = "python for beginners"; // when redeclaring a variable you don't need to use var again
+
+alert(course);
+
+var var1 = "30 30 30 30 30 30" //String
+var var2 = 30.5 //number/float
+
