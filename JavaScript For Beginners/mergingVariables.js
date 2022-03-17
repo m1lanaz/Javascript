1ae5ff9db723fceb4cab9fb04e8d18a5643fed42
Milana Zaborovska <Milana.zaborovska@gmail.com>
2022-03-17 09:48:09 +0000

    created variables and alerts

---
+++ JavaScript For Beginners/mergingVariables.js
@@ -0,0 +1,7 @@
+var intro = "Hi my name is Milana and I am _____ years old";
+var age = 16;
+intro = "Hi my name is Milana and I am " + age + " years old";
+alert(intro);
+
+var num2 = 40.123;
+alert(age + "" + num2); //This is no longer a number, it is a string
